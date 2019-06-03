9bb1b81829484f118767a58e3d6366706566fe2f
Adam Wilson <adamwilson1234@gmail.com>
2019-06-03 17:38:55 -0400

    solutions.component now has a searchable table of solutions coming from fake data. Sorting is not working.Data is not of type Solutions[] yet because of a recursive property

--- src/app/Components/solutions/solutions.component.ts
+++ src/app/Components/solutions/solutions.component.ts
@@ -1,36 +1,288 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
-import { SaasafrasService } from '../../services/saasafras.service';
-import { Solutions, Solution } from 'src/model/saasafras/solution';
-import { Observable } from 'rxjs';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+import { SaasafrasService } from "../../services/saasafras.service";
+import { Solutions, Solution } from "src/model/saasafras/solution";
+import { Observable } from "rxjs";
+import { MatTableDataSource } from "@angular/material";
+import { MatSortModule } from "@angular/material/sort";
 
 @Component({
-  selector: 'app-solutions',
-  templateUrl: './solutions.component.html',
-  styleUrls: ['./solutions.component.css']
+    selector: "app-solutions",
+    templateUrl: "./solutions.component.html",
+    styleUrls: ["./solutions.component.css"]
 })
-export class SolutionsComponent implements OnInit { 
-  solutions: Solution[];
-  newSolution = new Solution(null, 'New Solution', '0.0', null);
-  showNewSolution: boolean;
-  showNewIcon = 'expand_more';
+export class SolutionsComponent implements OnInit {
+    // TODO REMOVED Solution type enforcement
+    // as Application is recursive
+    solutions = this.getFakeData();
 
-  mySolutions: Observable<Solutions>;
+    // MATERIAL TABLE SETUP
+    displayedColumns: string[] = [
+        "action",
+        "name",
+        "version",
+        "creationDate",
+        "lastUpdateDate",
+        "description"
+    ];
+    dataSource = new MatTableDataSource(this.solutions);
+    @ViewChild(MatSortModule) sort: MatSortModule;
 
-  constructor(private _authService: AuthService,
-     private _solutionService: SaasafrasService) {
-       this.mySolutions = this._solutionService.GetSolutions();
-       this.mySolutions.subscribe({next: (s: Solutions) => {
-         this.solutions = s.solutions;
-        //  this.solutions.unshift(this.newSolution); // optionally add a new solution to the top
-       }});
-     }
+    newSolution = new Solution(null, "New Solution", "0.0", null);
+    showNewSolution: boolean;
+    showNewIcon = "expand_more";
 
-  toggleNewSolution(show: boolean) {
-    console.log('settings showNewSolution to: ' + show);
-    this.showNewSolution = show;
-    this.showNewIcon = show ? 'expand_less' : 'expand_more';
-  }
-  ngOnInit() {
-  }
+    mySolutions: Observable<Solutions>;
+
+    constructor(
+        private _authService: AuthService,
+        private _solutionService: SaasafrasService
+    ) {
+        // TODO GET REAL DATA
+        // this.mySolutions = this._solutionService.GetSolutions();
+        // this.mySolutions.subscribe({
+        //     next: (s: Solutions) => {
+        //         this.solutions = s.solutions;
+        //         //  this.solutions.unshift(this.newSolution); // optionally add a new solution to the top
+        //     }
+        // });
+    }
+
+    ngOnInit() {
+        // TODO SORT NOT WORKING
+        this.dataSource.sort = this.sort;
+    }
+
+    applyFilter(filterValue: string) {
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
+
+    toggleNewSolution(show: boolean) {
+        console.log("settings showNewSolution to: " + show);
+        this.showNewSolution = show;
+        this.showNewIcon = show ? "expand_less" : "expand_more";
+    }
+
+    getFakeData() {
+        const fakeApplication = {
+            appId: 123,
+            appName: "foo",
+            podioApplication: {
+                appId: 123,
+                appName: "foo",
+                podioApplication: "cant recurse here",
+                fields: ["foo", "bar"]
+            },
+            fields: ["foo", "bar"]
+        };
+        const fakeWorkspace = {
+            workspaceName: "Workspace 1",
+            workspaceId: 123,
+            apps: [fakeApplication],
+            podioSpace: {
+                // strings
+                org_id: "123",
+                space_id: "123",
+                name: "podio space",
+                rights: ["foo", "bar"],
+                role: "role",
+                type: "type",
+                url: "https://someurl.com",
+                url_label: "url label",
+                privacy: "privacy",
+                created_on: new Date(),
+                description: "description",
+                created_by: "any",
+                // type Owner
+                owner: { id: "123", type: "type" },
+                // booleans
+                premium: false,
+                post_on_new_app: false,
+                post_on_new_member: false,
+                auto_join: false,
+                is_overdue: false
+            }
+        };
+
+        const fakeSolutions = [
+            {
+                action: "action",
+                name: "sol 1 ",
+                version: 1,
+                creationDate: new Date("2015-05-05"),
+                lastUpdateDate: new Date(),
+                description: "my lollipop"
+            },
+            {
+                action: "action",
+                name: "sol 2",
+                version: 2,
+                creationDate: new Date("2017-05-05"),
+                lastUpdateDate: new Date(),
+                description: "my ice cream"
+            },
+            {
+                action: "action",
+                name: "sol 3",
+                version: 1,
+                creationDate: new Date("2016-05-05"),
+                lastUpdateDate: new Date(),
+                description: "my burger"
+            },
+            {
+                action: "action",
+                name: "sol 4",
+                version: 4,
+                creationDate: new Date("2018-05-05"),
+                lastUpdateDate: new Date(),
+                description: "my pizza"
+            },
+            {
+                action: "action",
+                name: "sol 4",
+                version: 4,
+                creationDate: new Date("2018-05-05"),
+                lastUpdateDate: new Date(),
+                description: "my roasted pig"
+            },
+            {
+                action: "action",
+                name: "sol 4",
+                version: 4,
+                creationDate: new Date("2018-05-05"),
+                lastUpdateDate: new Date(),
+                description: "my description"
+            },
+            {
+                action: "action",
+                name: "sol 4",
+                version: 4,
+                creationDate: new Date("2018-05-05"),
+                lastUpdateDate: new Date(),
+                description: "my description"
+            },
+            {
+                action: "action",
+                name: "sol 4",
+                version: 4,
+                creationDate: new Date("2018-05-05"),
+                lastUpdateDate: new Date(),
+                description: "my description"
+            },
+            {
+                action: "action",
+                name: "sol 4",
+                version: 4,
+                creationDate: new Date("2018-05-05"),
+                lastUpdateDate: new Date(),
+                description: "my description"
+            },
+            {
+                action: "action",
+                name: "sol 4",
+                version: 4,
+                creationDate: new Date("2018-05-05"),
+                lastUpdateDate: new Date(),
+                description: "my description"
+            },
+            {
+                action: "action",
+                name: "sol 4",
+                version: 4,
+                creationDate: new Date("2018-05-05"),
+                lastUpdateDate: new Date(),
+                description: "my description"
+            },
+            {
+                action: "action",
+                name: "sol 4",
+                version: 4,
+                creationDate: new Date("2018-05-05"),
+                lastUpdateDate: new Date(),
+                description: "my description"
+            },
+            {
+                action: "action",
+                name: "sol 4",
+                version: 4,
+                creationDate: new Date("2018-05-05"),
+                lastUpdateDate: new Date(),
+                description: "my description"
+            },
+            {
+                action: "action",
+                name: "sol 4",
+                version: 4,
+                creationDate: new Date("2018-05-05"),
+                lastUpdateDate: new Date(),
+                description: "my description"
+            },
+            {
+                action: "action",
+                name: "sol 4",
+                version: 4,
+                creationDate: new Date("2018-05-05"),
+                lastUpdateDate: new Date(),
+                description: "my description"
+            },
+            {
+                action: "action",
+                name: "sol 4",
+                version: 4,
+                creationDate: new Date("2018-05-05"),
+                lastUpdateDate: new Date(),
+                description: "my description"
+            }
+        ];
+
+        return fakeSolutions;
+    }
 }
+
+/**
+ * MODELS:
+ *
+ * 
+ * export class Solution {
+    constructor(
+        public appId: string,
+        public name: string,
+        public version: string,
+        public workspaces: $Space[]) { }
+}
+
+export class $Space {
+    constructor(
+        public workspaceName: string,
+        public workspaceId: Number,
+        public podioSpace: Space,
+        public apps: $Application[]) { }
+}
+
+export class Space {
+    public org_id: string;
+    public space_id: string;
+    public name: string;
+    public rights: string[];
+    public role: string;
+    public type: string;
+    public url: string;
+    public url_label: string;
+    public privacy: string;
+    public premium: boolean;
+    public post_on_new_app: boolean;
+    public post_on_new_member: boolean;
+    public auto_join: boolean;
+    public created_on: string;
+    public description: string;
+    public is_overdue: boolean;
+    public created_by: any;
+    public owner: Owner;
+}
+
+export class Owner {
+    public id: string;
+    public type: string;
+}
+
+ */
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
-
+/** Angular */
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
+import { RouterModule } from "@angular/router";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { CustomMaterialModule } from "./custom-material/custom-material.module";
+
+/** Components */
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
-import { APP_ROUTES } from './app.routes';
 import { SaasafrasMenuComponent } from './Components/user-icon/user-icon.component';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { FeedbackComponent } from './Components/feedback/feedback.component';
@@ -14,14 +19,10 @@ import { FooterComponent } from './Components/footer/footer.component';
 import { VersionresultsComponent } from './Components/versionresults/versionresults.component';
 import { AuthenticatingComponent } from './Components/authenticating/authenticating.component';
 import { FeedbackBoxComponent } from './Components/feedback-box/feedback-box.component';
-import { RouterModule } from '@angular/router';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AuthViewComponent } from './auth-view/auth-view.component';
 import { LoginComponent } from './Components/login/login.component';
 import { PodioCredentialComponent } from './podio-credential/podio-credential.component';
-import { FormsModule } from '@angular/forms';
 import { ServiceAuthenticatingComponent } from './service-authenticating/service-authenticating.component';
-import { CustomMaterialModule } from './custom-material/custom-material.module';
 import { SolutionsComponent } from './Components/solutions/solutions.component';
 import { TaskListComponent } from './Components/task-list/task-list.component';
 import { PodioSpacesComponent } from './podio-spaces/podio-spaces.component';
@@ -38,48 +39,54 @@ import { NewSolutionComponent } from './new-solution/new-solution.component';
 import { EditSolutionComponent } from './edit-solution/edit-solution.component';
 import { DeploySolutionComponent } from './deploy-solution/deploy-solution.component';
 
+/** Other */
+import { APP_ROUTES } from "./app.routes";
+
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    PageNotFoundComponent,
-    SaasafrasMenuComponent,
-    NavbarComponent,
-    FeedbackComponent,
-    AccountsComponent,
-    SettingsComponent,
-    FooterComponent,
-    VersionresultsComponent,
-    AuthenticatingComponent,
-    FeedbackBoxComponent,
-    AuthViewComponent,
-    PodioCredentialComponent,
-    ServiceAuthenticatingComponent,
-    SolutionsComponent,
-    TaskListComponent,
-    PodioSpacesComponent,
-    PodioOrgsComponent,
-    PodioOrgComponent,
-    PodioSpaceComponent,
-    SolutionComponent,
-    SaasSpaceComponent,
-    SaasApplicationComponent,
-    SaasFieldComponent,
-    PodioApplicationComponent,
-    SaasSpacesComponent,
-    NewSolutionComponent,
-    EditSolutionComponent,
-    DeploySolutionComponent
-  ],
-  imports: [
-    FormsModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    CustomMaterialModule,
-    RouterModule.forRoot(APP_ROUTES),
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        /** Components */
+        AppComponent,
+        LoginComponent,
+        PageNotFoundComponent,
+        SaasafrasMenuComponent,
+        NavbarComponent,
+        FeedbackComponent,
+        AccountsComponent,
+        SettingsComponent,
+        FooterComponent,
+        VersionresultsComponent,
+        AuthenticatingComponent,
+        FeedbackBoxComponent,
+        AuthViewComponent,
+        PodioCredentialComponent,
+        ServiceAuthenticatingComponent,
+        SolutionsComponent,
+        TaskListComponent,
+        PodioSpacesComponent,
+        PodioOrgsComponent,
+        PodioOrgComponent,
+        PodioSpaceComponent,
+        SolutionComponent,
+        SaasSpaceComponent,
+        SaasApplicationComponent,
+        SaasFieldComponent,
+        PodioApplicationComponent,
+        SaasSpacesComponent,
+        NewSolutionComponent,
+        EditSolutionComponent,
+        DeploySolutionComponent
+    ],
+    imports: [
+        /** Modules */
+        FormsModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        CustomMaterialModule,
+        RouterModule.forRoot(APP_ROUTES),
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
