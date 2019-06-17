f3a946714a6616ed0cbbc654c8f153827d3b562f
Adam Wilson <adamwilson1234@gmail.com>
2019-06-17 16:18:06 -0400

    added custom styled tooltip that shows and hides programatically to welcome page

--- src/app/Components/welcome/welcome.component.ts
+++ src/app/Components/welcome/welcome.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { MatTooltip } from "@angular/material";
+import { TooltipPosition } from "@angular/material/tooltip";
+
+
 
 @Component({
     selector: "app-welcome",
@@ -7,36 +11,40 @@ import { Component, OnInit } from "@angular/core";
 })
 export class WelcomeComponent implements OnInit {
     // items ie card items have: name, imageUrl, description,
-
+    @ViewChild("tooltip") tooltip;
     onboardingOptions;
 
     constructor() {}
 
     ngOnInit() {
-         this.onboardingOptions = [
-             {
-                 name: "Let's build something!",
-                 featured: true,
-                 iconName: "build",
-                 description:
-                     "Let's pick some Podio workspaces and create a Solution!",
-                 linkUrl: "/podio"
-             },
-             {
-                 name: "About Saasafras",
-                 iconName: "play_circle_outline",
-                 description:
-                     "What problems can Saasafras solve? (2 min video)",
-                 linkUrl: "/podio"
-             },
-             {
-                 name: "Browse Around",
-                 iconName: "directions_walk",
-                 description: "Just wanna click around for now",
-                 linkUrl: "/podio"
-             }
-         ];
+        this.onboardingOptions = [
+            {
+                name: "Let's build something!",
+                featured: true,
+                iconName: "build",
+                description:
+                    "Let's pick some Podio workspaces and create a Solution!",
+                linkUrl: "/podio"
+            },
+            {
+                name: "About Saasafras",
+                iconName: "play_circle_outline",
+                description: "What problems can Saasafras solve? (2 min video)",
+                linkUrl: "/podio"
+            },
+            {
+                name: "Browse Around",
+                iconName: "directions_walk",
+                description: "Just wanna click around for now",
+                linkUrl: "/podio"
+            }
+        ];
 
-         console.log(this.onboardingOptions)
+        console.log(this.onboardingOptions);
+    }
+
+    ngAfterViewInit() {
+        this.tooltip.show(0);
+        console.log('after view init')
     }
 }
